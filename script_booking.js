c4cce007e3c58205fac0ed1341e0e673ebf9c6cd
Balázs Szeidl <szeidl.balazs@hotmail.com>
2020-12-03 12:01:53 +0100

    formázás finomítva, alap visszajelző üzenet bent

--- script_booking.js
+++ script_booking.js
@@ -6,15 +6,23 @@ function checkDate() {
 	if (inputDate.getTime() <= -86400000 + today.getTime()) {
 		alert("A megadott dátum nem lehet régebbi a mai dátumnál!");
 	}
-}
+} 
 
 
- function checkTime() {
-	var currentTime = get.Time();
-	var inputTime = get.time(document.getElementById("myTime").value);
-	if (inputTime.getTime() < currentTime.getTime()) {
+
+function checkTime(){
+	let timeNow = new Date();
+	let tm = document.getElementById("myTime");
+	let timeParts = tm.split(":");
+	let inputTime = new Date(timeNow.getYear(), timeNow.getMonth(), timeNow.getDate(), parseInt(timeParts[0]), parseInt(timeParts[1]), 0, 0);
+
+	let diff = Math.abs(inputTime.getTime() - timeNow.getTime());
+
+	if (diff < 3600) {
 		alert("Legalább 1 órával korábban foglaljon asztalt!");
 	}
 }
 
-
+function orderSummary() {
+	alert("Köszönjük az asztal foglalását, szeretettel várjuk a lefoglalt időpontban");
+}
