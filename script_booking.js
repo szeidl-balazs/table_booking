7b9713670804bccb368377ab3fa8c261fa84b22e
Balázs Szeidl <szeidl.balazs@hotmail.com>
2020-12-03 13:23:08 +0100

    input values print ready but disappear

--- script_booking.js
+++ script_booking.js
@@ -12,17 +12,28 @@ function checkDate() {
 
 function checkTime(){
 	let timeNow = new Date();
-	let tm = document.getElementById("myTime");
+	let tm = document.getElementById("myTime").value;
 	let timeParts = tm.split(":");
 	let inputTime = new Date(timeNow.getYear(), timeNow.getMonth(), timeNow.getDate(), parseInt(timeParts[0]), parseInt(timeParts[1]), 0, 0);
 
 	let diff = Math.abs(inputTime.getTime() - timeNow.getTime());
 
-	if (diff < 3600) {
+	if (diff < 3600000) {
 		alert("Legalább 1 órával korábban foglaljon asztalt!");
 	}
 }
 
 function orderSummary() {
-	alert("Köszönjük az asztal foglalását, szeretettel várjuk a lefoglalt időpontban");
+let name = document.getElementById("myName").value;
+let email = document.getElementById("myEmail").value;
+let date = document.getElementById("myDate").value;
+let time = document.getElementById("myTime").value;
+let guest = document.getElementById("numberOfPeople").value;
+
+document.getElementById("checkoutText").innerHTML = "Köszönjük az asztal foglalását, szeretettel várjuk a lefoglalt időpontban.";
+document.getElementById("checkoutName").innerHTML = name;
+document.getElementById("checkoutEmail").innerHTML = email;
+document.getElementById("checkoutDate").innerHTML = date;
+document.getElementById("checkoutTime").innerHTML = time;
+document.getElementById("checkoutGuest").innerHTML = guest;
 }
