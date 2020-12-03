6a223a270eb7caeb9b64a2c2a4e754cd23bd6f97
richargold04 <richard.gold04@gmail.com>
2020-12-03 19:49:22 +0100

    js added

--- script_booking.js
+++ script_booking.js
@@ -1,39 +1,57 @@
-/*compare date*/
+function test() {
 
-function checkDate() {
-	var today = new Date();	
-	var inputDate = new Date(document.getElementById("myDate").value);
-	if (inputDate.getTime() <= -86400000 + today.getTime()) {
-		alert("A megadott dátum nem lehet régebbi a mai dátumnál!");
+  var element = document.getElementById("myTime").value;
+  
+  if (element == "") {
+  alert("Please Enter Time");
+    return false;  
+  }
+  else {
+  
+  // get system local time
+  var d = new Date();
+  var m = d.getMinutes();
+  var h = d.getHours();
+  if(h == '0') {h = 24}
+  
+  var currentTime = h+"."+m;
+  console.log(currentTime);
+ 
+  // get input time
+  var time = element.split(":");
+  var hour = time[0];
+  if(hour == '00') {hour = 24}
+  var min = time[1];
+  
+  var inputTime = hour+"."+min;
+  console.log(inputTime);
+  
+  var totalTime = currentTime - inputTime;
+  console.log(totalTime);
+  
+  if ((Math.abs(totalTime)) < 1 || inputDate < today ) {
+    alert("Legalább 1 órával korábban foglaljon asztalt!");
+	} else {
+		var today = new Date();	
+		var inputDate = new Date(document.getElementById("myDate").value);
+		if (inputDate.getTime() <= -86400000 + today.getTime() && inputTime < currentTime ) {
+			alert("A megadott dátum nem lehet régebbi a mai dátumnál!");	
 	}
-} 
-
-
-
-function checkTime(){
-	let timeNow = new Date();
-	let tm = document.getElementById("myTime").value;
-	let timeParts = tm.split(":");
-	let inputTime = new Date(timeNow.getYear(), timeNow.getMonth(), timeNow.getDate(), parseInt(timeParts[0]), parseInt(timeParts[1]), 0, 0);
-
-	let diff = Math.abs(inputTime.getTime() - timeNow.getTime());
-
-	if (diff < 3600000) {
-		alert("Legalább 1 órával korábban foglaljon asztalt!");
+	else {
+		document.getElementById("checkoutText").innerHTML = "Köszönjük az asztal foglalását, szeretettel várjuk a lefoglalt időpontban.";
+		
+			let name = document.getElementById("myName").value;
+			let email = document.getElementById("myEmail").value;
+			let date = document.getElementById("myDate").value;
+			let time = document.getElementById("myTime").value;
+			let guest = document.getElementById("numberOfPeople").value;
+		
+			document.getElementById("checkoutName").innerHTML = "Név: " + name;
+			document.getElementById("checkoutEmail").innerHTML = "Email cím: " + email;
+			document.getElementById("checkoutDate").innerHTML = "Dátum: " + date;
+			document.getElementById("checkoutTime").innerHTML = "Időpont: " + time;
+			document.getElementById("checkoutGuest").innerHTML = "Vendégek száma: " + guest;
+		
+			console.log(name + "\n", email + "\n", date + "\n", time + "\n", guest)
 	}
-}
-
-function orderSummary() {
-let name = document.getElementById("myName").value;
-let email = document.getElementById("myEmail").value;
-let date = document.getElementById("myDate").value;
-let time = document.getElementById("myTime").value;
-let guest = document.getElementById("numberOfPeople").value;
-
-document.getElementById("checkoutText").innerHTML = "Köszönjük az asztal foglalását, szeretettel várjuk a lefoglalt időpontban.";
-document.getElementById("checkoutName").innerHTML = name;
-document.getElementById("checkoutEmail").innerHTML = email;
-document.getElementById("checkoutDate").innerHTML = date;
-document.getElementById("checkoutTime").innerHTML = time;
-document.getElementById("checkoutGuest").innerHTML = guest;
-}
+	}}}
